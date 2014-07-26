3d9a6c08577f0069a28d1a302d468fc3e17d6886
justinvdm <justinvdm.0.1.0@gmail.com>
2014-07-26 15:07:40 +0200

    implement the basics

--- multifn.js
+++ multifn.js
@@ -1,5 +1,32 @@
 (function() {
-  function multifn() {}
+  function multifn() {
+    var types = [];
+
+    function _multifn_() {
+      var i = -1;
+      var n = types.length;
+      var type;
+
+      while (++i < n) {
+        type = types[i];
+        if (!type.if.apply(this, arguments)) continue;
+        return type.do.apply(this, arguments);
+      }
+    }
+
+    _multifn_.if = function(ifFn) {
+      return {do: function(doFn) {
+        types.push({
+          if: ifFn,
+          do: doFn
+        });
+
+        return _multifn_;
+      }};
+    };
+
+    return _multifn_;
+  }
 
   if (typeof module != 'undefined')
     module.exports = multifn;
--- multifn.test.js
+++ multifn.test.js
@@ -1,2 +1,31 @@
+var assert = require('assert');
+var multifn = require('./multifn');
+
+
 describe("multifn", function() {
+  it("should use the first matching override", function() {
+    var fn = multifn()
+      .if(function(a, b) { return a && !b; })
+      .do(function(a) { return a; })
+
+      .if(function(a, b) { return a && b; })
+      .do(function(a, b) { return a + b; })
+
+      .if(function(a, b) { return a && b; })
+      .do(function() { return 3; });
+
+    assert.equal(fn(2), 2);
+    assert.equal(fn(4), 4);
+    assert.equal(fn(1, 3), 4);
+    assert.equal(fn(2, 3), 5);
+  });
+
+  it("should return undefined if there are no matching functions", function() {
+    var fn = multifn()
+      .if(function(a) { return a; })
+      .do(function() { return 23; });
+
+    assert.equal(fn(true), 23);
+    assert(typeof fn(false) == 'undefined');
+  });
 });
